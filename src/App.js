0d6624240fb87cdaf95621d76805ddf85e5389b8
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-26 14:59:22 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import Cart from './components/Cart/Cart'
+import Layout from './components/Layout/Layout'
+import Products from './components/Shop/Products';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true
 
 function App() {
+  const dispatch = useDispatch()
+  const showCart = useSelector((state) => state.ui.cartIsVisible)
+  const cart = useSelector((state) => state.cart)
+  const notification = useSelector((state) => state.ui.notification)
+
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: 'pending',
+        title: 'Sending...',
+        message: 'Sending cart data.'
+      }))
+      const response = await fetch(
+        'https://http-practice-68dcb-default-rtdb.firebaseio.com/cart.json',
+        {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      })
+
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.')
+      }
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success',
+        message: 'Cart data sent.'
+      }))
+    }
+
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error.',
+        message: 'Cart data failed to send.'
+      }))
+    })
+  }, [cart, dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      {notification && <Notification
+      status={notification.status}
+      title={notification.title}
+      message={notification.message}
+      />}
+    <Layout>
+      {showCart && <Cart />}
+      <Products />
+    </Layout>
+    </Fragment>
   );
 }
 
